d46cc92456651626bf18dc092650f2a5f74283bf
simon <stockfisch97@gmail.com>
2022-01-03 17:55:13 +0100

    Scripts, about & navbar
    - added an about page
    - added basic js
    - added a navbar

---
+++ js/loadCopyright.js
@@ -0,0 +1,4 @@
+document.getElementById("myFooter").innerHTML =
+"<link rel='stylesheet' href='css/footer.css'>"
+	+"<p class='copyright' id='copyright'>Copyright &copy; " + new Date().getFullYear() + " Simon Fr&uuml;hauf. All"
+	+ " rights reserved.</p>"
---
+++ js/loadNav.js
@@ -0,0 +1,8 @@
+document.getElementById("myNav").innerHTML =
+"<link rel='stylesheet' href='css/navbar.css'>"
++"<div id='navbar'>"
+    +"<a href='index.html'>Home</a>"
+    +"<a href=''>Other</a>"
+    +"<a href='about.html'>About</a>"
+    +"</div>"
+
