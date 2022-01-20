11c3d5e28b23eb5367a2f69fd350b46581b60d0b
simon <stockfisch97@gmail.com>
2022-01-20 17:32:14 +0100

    Updated navbar & main page content
    added baseline for social buttons, beautified navbar, rewrote index page

--- js/loadNav.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("myNav").innerHTML =
-"<link rel='stylesheet' href='css/navbar.css'>"
-+"<div id='navbar'>"
-    +"<a href='index.html'>Home</a>"
-    +"<a href=''>Other</a>"
-    +"<a href='about.html'>About</a>"
-    +"</div>"
-
