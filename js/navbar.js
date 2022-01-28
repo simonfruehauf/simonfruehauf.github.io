0a42757f32c5307bcce3704cad9db1637e6e0b63
simon <stockfisch97@gmail.com>
2022-01-28 12:06:28 +0100

    Added socials

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,16 @@
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
+var socials = document.getElementById("socials");
 
 var sticky = navbar.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
+    socials.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
+    socials.classList.remove("sticky")
   }
 }
