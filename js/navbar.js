b10ed855cc4ccebc0e3998b9938d2698329efc72
simon <stockfisch97@gmail.com>
2022-02-02 10:37:00 +0100

    Reworked navbar & added mobile navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,16 +1,9 @@
-window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("navbar");
-var socials = document.getElementById("socials");
-
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-    socials.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-    socials.classList.remove("sticky")
+function toggleNav() {
+    var x = document.getElementById("navbar");
+    if (x.className === "mob-navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "mob-navbar";
+    }
   }
-}
