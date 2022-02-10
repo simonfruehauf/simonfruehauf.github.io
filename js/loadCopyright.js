192d7024371e6d86e19493261ae7bc8771ce9d23
simon <stockfisch97@gmail.com>
2022-02-10 11:32:55 +0100

    Fixed navbar height

--- js/loadCopyright.js
+++ js/loadCopyright.js
@@ -1,4 +1,3 @@
 document.getElementById("myFooter").innerHTML =
-"<link rel='stylesheet' href='css/footer.css'>"
-	+"<p class='copyright' id='copyright'>Copyright &copy; " + new Date().getFullYear() + " Simon Fr&uuml;hauf. All"
+	"<p class='copyright' id='copyright'>Copyright &copy; " + new Date().getFullYear() + " Simon Fr&uuml;hauf. All"
 	+ " rights reserved.</p>"
