d431fb296a1e222df2b2d1ee4af66bba043d85d9
Simon <stockfisch97@gmail.com>
2022-03-18 11:18:48 +0100

    temporary submit for new everything

--- js/navbar.js
+++
@@ -1,9 +0,0 @@
-
-function toggleNav() {
-    var x = document.getElementById("navbar");
-    if (x.className === "mob-navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "mob-navbar";
-    }
-  }
