6a9bf172e821c482d61a6548608aa2df3548687d
Simon <stockfisch97@gmail.com>
2022-04-19 07:38:24 +0200

    Completely overhauled webpage

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,54 +1,22 @@
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
 
-darkmodebutton = document.getElementById('darkmode-button');
-darkmodeicon = darkmodebutton.firstChild;
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
 
-darkmodebutton.addEventListener('click', event => darkmode());
-
-function darkmode() {
-    var element = document.body;
-    element.classList.toggle("light-mode");
-
-    //darkmodebutton.innerHTML = '';
-    if (element.classList.contains("light-mode"))
-    {
-        darkmodeicon.innerHTML = "dark_mode";
-        
-        document.documentElement.style.setProperty('--button-color', "rgb(31, 31, 31)");
-        document.documentElement.style.setProperty('--gridcontainer-color', "rgb(38, 28, 18, 0.35)");
-        document.documentElement.style.setProperty('--button-font-color', "rgb(238, 237, 224)");
-        document.documentElement.style.setProperty('--button-font-color-almost', "rgb(31, 31, 31)");
-        document.documentElement.style.setProperty('--button-font-color-spoton', "rgb(31, 31, 31)");
-
-        document.documentElement.style.setProperty('--grid-color', "rgb(31, 31, 31)");
-        document.documentElement.style.setProperty('--grid-color-font', "rgb(31, 31, 31)");
-
-        document.documentElement.style.setProperty('--grid-color-active', "rgb(170, 170, 170)");
-        document.documentElement.style.setProperty('--grid-color-inactive', "rgb(110, 110, 110)");
-
-        document.documentElement.style.setProperty('--grid-color-spoton', "rgb(130, 241, 97)");
-        document.documentElement.style.setProperty('--grid-color-almost', "rgb(192, 175, 46)");
-
-        
-    }
-    else {
-
-        darkmodeicon.innerHTML = "light_mode";
-        document.documentElement.style.setProperty('--gridcontainer-color', "rgb(238, 228, 218, 0.35)");
-
-        document.documentElement.style.setProperty('--button-color', "rgb(238, 237, 224)");
-        document.documentElement.style.setProperty('--button-font-color', "rgb(31, 31, 31)");
-        document.documentElement.style.setProperty('--button-font-color-almost', "rgb(31, 31, 31)");
-        document.documentElement.style.setProperty('--button-font-color-spoton', "rgb(31, 31, 31)");
-
-        document.documentElement.style.setProperty('--grid-color', "rgb(31, 31, 31)");
-        document.documentElement.style.setProperty('--grid-color-font', "rgb(31, 31, 31)");
-
-        document.documentElement.style.setProperty('--grid-color-active', "rgb(210,210,210)");
-        document.documentElement.style.setProperty('--grid-color-inactive', "rgb(110, 110, 110)");
-
-        document.documentElement.style.setProperty('--grid-color-spoton', "rgb(130, 241, 97)");
-        document.documentElement.style.setProperty('--grid-color-almost', "rgb(236, 220, 97)");
-        
-
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
     }
 }
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
--- js/device.js
+++
@@ -1,8 +0,0 @@
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    var parent = document.body;
-    var sidebar = document.getElementById('sidebarcontainer');
-    document.getElementById('page1').prepend(document.getElementById('sidebarprofile'));
-    document.getElementById('sidebarcontainer').style.display = "none";
-} else {
-
-}
